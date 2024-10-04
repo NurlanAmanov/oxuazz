6aad19b4ffbb796401e6392a09bf336278bf301d
NurlanAmanov <nurlanemenov14@gmail.com>
2024-10-04 12:25:18 +0400

    passnew

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ function login() {
     let password = document.getElementById('password').value;
 
   
-    if (username === "admin" && password === "1234") {
+    if (username === "admin" && password === "oxu2024") {
       sessionStorage.setItem("loggedIn", "true");
       window.location.href = "dashbord.htm"; 
     } else {
